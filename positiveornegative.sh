f1ed76f9863977f2d76063517f4634ffd6fc18ff
Sahasra <sahasralalitha.nakka@gmail.com>
2026-02-11 20:03:03 +0530

    Add positiveornegative shell script

---
+++ positiveornegative.sh
@@ -0,0 +1,10 @@
+read -p "Enter a number: " number
+if [ $number -gt 0 ]
+then 
+    echo "The number is positive."
+elif [ $number -lt 0 ]
+then 
+    echo "The number is negative."
+else
+    echo "The number is zero."
+fi
